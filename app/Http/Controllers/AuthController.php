e15b22702887fe34d4010df242ed922509b28ab0
Kevin Tanusa Devara <kevin.tanusa.devara@gmail.com>
2021-11-04 23:23:55 +0700

    First Submit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'      => 'required|string',
+            'email'     => 'required|string|email|unique:users',
+            'password'  => 'required|string|confirmed',
+        ]);
+
+        $user = new User([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'Register Berhasil!'
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'     => 'required|string|email',
+            'password'  => 'required|string',
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials))
+        {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('MyTransaction');
+        $token = $tokenResult->token;
+
+        $token->save();
+
+        return response()->json([
+            'token' => $tokenResult->accessToken,
+            'token_type'=> 'Bearer',
+            'message' => 'Login Berhasil!'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Logout Berhasil!'
+        ]);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['prefix' => 'auth'], function() {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+
+        Route::get('transaction', 'TransactionController@index');
+        Route::get('transaction/{id}', 'TransactionController@show');
+        Route::post('transaction', 'TransactionController@store');
+        Route::put('transaction/{id}', 'TransactionController@update');
+        Route::delete('transaction/{id}', 'TransactionController@destroy');
+    });
+});
